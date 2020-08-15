6d5d8a6d00e9997f63ab389f28e066357ed3a0d9
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-15 01:15:24 -0700

    added drawableLeft

--- app/src/main/java/com/hanseltritama/keyframedemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/keyframedemo/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.TransitionManager
 import android.view.View
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.my_textview
+import kotlinx.android.synthetic.main.activity_main_second.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +22,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        my_textview.setDrawableLeft(R.drawable.ic_account_circle)
+
         layout = findViewById(R.id.my_layout)
 
         constraintSetHide.clone(layout)
         constraintSetShow.clone(this, R.layout.activity_main_second)
 
+    }
+
+    private fun TextView.setDrawableLeft(drawable: Int) {
+        this.setCompoundDrawablesRelativeWithIntrinsicBounds(drawable, 0, 0, 0)
     }
 
     fun handleKeyframe(view: View) {
